updated sharpe formula and fixed sharpe computation in model_GAN.ipynb. Correct sharpe expression should be sharpe(1-SDF)* sqrt(12)

src/utils.py
@@ -30,7 +30,7 @@ def Markowitz(r):
 
 
 def sharpe(r):
-    return np.mean(r / r.std())
+    return np.mean(r) / r.std()
 
 
 def load_sorted_results(path, by=None):